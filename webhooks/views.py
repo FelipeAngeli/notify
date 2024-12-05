8e1541b51cb3d9c1ee642465ad3f4875c19619fb
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-12-05 11:18:14 -0300

    callmebot mensage

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,4 +1,8 @@
+import json
 from rest_framework import views, response, status
+from webhooks.models import Webhook
+from webhooks.message import outflow_message
+from services.callmebot import CallMeBot
 
 
 class WebhookView(views.APIView):
@@ -6,7 +10,26 @@ class WebhookView(views.APIView):
     def post(self, request):
         data = request.data
         
-        print(data)
+        Webhook.objects.create(
+            event_type=data.get('event_type'),
+            event=json.dumps(data, ensure_ascii=False),
+        )
+
+        product_name = data.get('product')
+        quantity = data.get('quantity')
+        product_cost_price = data.get('product_cost_price')
+        product_selling_price = data.get('product_selling_price')
+        total_value = quantity * product_selling_price
+        profit_value = total_value - (quantity * product_cost_price)
+
+        message = outflow_message.format(
+            product_name,
+            quantity,
+            total_value,
+            profit_value
+        )
+        callmebot = CallMeBot()
+        callmebot.send_message(message)
 
         return response.Response(
             data=data,
